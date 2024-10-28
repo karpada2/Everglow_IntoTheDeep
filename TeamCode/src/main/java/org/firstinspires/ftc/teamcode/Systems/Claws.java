51035d6b894c72388f380a63311f09ede929ce18
karpada2 <gongrossman@gmail.com>
2024-10-28 20:25:23 +0200

    finished initial work on claws

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Claws.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Claws.java
@@ -12,18 +12,31 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class Claws {
 
-
+    Servo clawServo;
+    Servo rotateServo;
 
     private class ClawAction implements Action {
-        private double clawDestination;
+        private final double clawDestination;
+        private final boolean isRotate;
 
         public ClawAction(double clawDestination) {
             this.clawDestination = clawDestination;
+            isRotate = false;
+        }
+
+        public ClawAction(double clawDestination, boolean isRotate) {
+            this.clawDestination = clawDestination;
+            this.isRotate = isRotate;
         }
 
         @Override
         public boolean run(@NonNull TelemetryPacket telemetryPacket) {
-            setClawState(clawDestination);
+            if (isRotate) {
+                setRotateServo(clawDestination);
+            }
+            else {
+                setClawState(clawDestination);
+            }
             return true;
         }
 
@@ -34,29 +47,48 @@ public class Claws {
     }
 
     enum ClawState {
-        CLOSE(1),
-        OPEN(0);
+        CLAW_CLOSE(1),
+        CLAW_OPEN(0),
+        ROTATE_CLOSE(1),
+        ROTATE_OPEN(0);
 
         public final int state;
 
         ClawState(int state) {this.state = state;}
     }
 
-    Servo clawServo;
-    Servo rotateServo;
-
     public Claws(OpMode opMode) {
         clawServo = opMode.hardwareMap.get(Servo.class, "clawServo");
         rotateServo = opMode.hardwareMap.get(Servo.class, "rotateServo");
-        setClawState(ClawState.OPEN.state);
+        setClawState(ClawState.CLAW_OPEN.state);
+        setRotateServo(ClawState.ROTATE_OPEN.state);
     }
 
-    public void toggleClaw() {
-        if (clawServo.getPosition() == ClawState.OPEN.state) {
-            clawServo.setPosition(ClawState.CLOSE.state);
+    // toggles the rotating servo or the claw servo based on isRotate
+    public void toggle(boolean isRotate) {
+        if (isRotate) {
+            toggleRotate();
         }
         else {
-            clawServo.setPosition(ClawState.CLOSE.state);
+            toggleClaw();
+        }
+    }
+
+    private void toggleClaw() {
+        if (clawServo.getPosition() == ClawState.CLAW_OPEN.state) {
+            clawServo.setPosition(ClawState.CLAW_CLOSE.state);
+        }
+        else {
+            clawServo.setPosition(ClawState.CLAW_OPEN.state);
+        }
+    }
+
+    private void toggleRotate() {
+        if (rotateServo.getPosition() == ClawState.ROTATE_OPEN.state) {
+            rotateServo.setPosition(ClawState.ROTATE_CLOSE.state);
+        }
+        else {
+            rotateServo.setPosition(ClawState.ROTATE_OPEN.state);
         }
     }
 
@@ -66,5 +98,9 @@ public class Claws {
 
     public Action moveClaw(double dest) {
         return new ClawAction(dest);
+    }
+
+    public void setRotateServo(double state) {
+        rotateServo.setPosition(state);
     }
 }
