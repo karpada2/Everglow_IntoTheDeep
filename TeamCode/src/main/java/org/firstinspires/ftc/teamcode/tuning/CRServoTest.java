58b4502657d0a7fe686fb018edf1853f3aff44cd
BuilderOfOz <150072067+aviv-nachum@users.noreply.github.com>
2024-11-04 21:42:56 +0200

    add tests for motors and servos

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/CRServoTest.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.tuning;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+@TeleOp(name = "Servo Test")
+public class CRServoTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        CRServo servo = hardwareMap.get(CRServo.class, "servo");
+
+        waitForStart();
+        while (opModeIsActive()) {
+            if (gamepad1.cross) {
+                servo.setPower(0.8);
+            }
+            else if (gamepad1.circle) {
+                servo.setPower(-0.8);
+            }
+            else {
+                servo.setPower(0);
+            }
+        }
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/MotorTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/MotorTester.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.tuning;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
+@TeleOp(name = "Motor test")
 public class MotorTester extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
@@ -13,6 +15,8 @@ public class MotorTester extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
+            telemetry.addData("is x pressed?", gamepad1.x);
+            telemetry.update();
             if (gamepad1.x){
                 motor.setPower(0.8);
             } else {
