4670cc20a27300feb7dc0c56532920b39c3b1de3
BuilderOfOz <150072067+aviv-nachum@users.noreply.github.com>
2024-11-11 21:23:54 +0200

    new op mode starting

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/NewOpMode.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.OpMode;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+public class NewOpMode extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            drive.setDrivePowers(new PoseVelocity2d(
+                    new Vector2d(
+                            -gamepad1.left_stick_y,
+                            -gamepad1.left_stick_x
+                    ),
+                    -gamepad1.right_stick_x
+            ));
+
+            drive.updatePoseEstimate();
+
+
+
+    }
+}
