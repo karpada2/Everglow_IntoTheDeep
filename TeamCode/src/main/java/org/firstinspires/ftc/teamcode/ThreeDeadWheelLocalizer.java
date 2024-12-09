089b77703cf4c2117e5dc0ccbed3d3493833dd65
BuilderOfOz <150072067+aviv-nachum@users.noreply.github.com>
2024-12-09 20:30:05 +0200

    repositioning of deadwheels

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThreeDeadWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThreeDeadWheelLocalizer.java
@@ -20,9 +20,9 @@ import org.firstinspires.ftc.teamcode.messages.ThreeDeadWheelInputsMessage;
 @Config
 public final class ThreeDeadWheelLocalizer implements Localizer {
     public static class Params {
-        public double par0YTicks = 8.26772/(2*1.88976*Math.PI/2000); // y position of the first parallel encoder (in tick units)
+        public double par0YTicks = 8.55/(2*0.00298641); // y position of the first parallel encoder (in tick units)
         public double par1YTicks = -par0YTicks; // y position of the second parallel encoder (in tick units)
-        public double perpXTicks = 8/(2*1.88976*Math.PI/2000); // x position of the perpendicular encoder (in tick units)
+        public double perpXTicks = 0; // x position of the perpendicular encoder (in tick units)
     }
 
     public static Params PARAMS = new Params();
