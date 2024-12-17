69b744a07a7d49e9a3fd65e01272b54c0aecef2a
karpada2 <gongrossman@gmail.com>
2024-12-17 20:12:59 +0200

    added very simple opMode to test the differential claws

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/DifferentialClawBaseTestOpMode.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.OpMode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+@TeleOp(name="very simple op mode for the differential claws")
+public class DifferentialClawBaseTestOpMode extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        CRServo servo1 = hardwareMap.get(CRServo.class, "testClawServo1");
+        CRServo servo2 = hardwareMap.get(CRServo.class, "testClawServo2");
+        waitForStart();
+        while (opModeIsActive()) {
+            servo1.setPower(gamepad1.left_stick_y/2);
+            servo2.setPower(gamepad1.right_stick_y/2);
+        }
+    }
+}
