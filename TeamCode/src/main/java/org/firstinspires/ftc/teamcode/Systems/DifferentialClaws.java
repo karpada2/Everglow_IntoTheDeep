f0242e00fc8d1763a071f93e5572ee8f9dc45cce
Omri7r <omri.r2007@gmail.com>
2025-01-13 20:36:17 +0200

    start handling Actions with claws

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/DifferentialClaws.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/DifferentialClaws.java
@@ -15,6 +15,7 @@ import androidx.annotation.NonNull;
 import com.acmerobotics.dashboard.canvas.Canvas;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
+import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -24,10 +25,6 @@ public class DifferentialClaws {
 
     public static final double holdingPower = 0.15;
 
-    public static double getRotationOfInput(AnalogInput input) {
-        return (input.getVoltage() / input.getMaxVoltage()) * 360;
-    }
-
     CRServo leftClawServo;
     CRServo rightClawServo;
     AnalogInput clawInput1;
@@ -36,51 +33,58 @@ public class DifferentialClaws {
     double armPosition = 0;
     double lastPosRequest = 0;
 
+    boolean isGoingDown = false;
+
     // tracks from -∞ - ∞ the rotation of each motor.
-    double leftClawServoRotation = 0;
-    double rightClawServoRotation = 0;
+    //double leftClawServoRotation = 0;
+    //double rightClawServoRotation = 0;
+
+    private double leftClawOldPos;
+    private double rightClawOldPos;
+
+    private final double leftClawStart;
+    private final double rightClawStart;
+
+    private double trueLeftRotation = 0;
+    private double trueRightRotation = 0;
+
+    public PIDController controller;
+
+    public final double p = 0.01, i = 0.01, d = 0.0001;
+    public final double f = 0.1;
+
+    private double target = 0;
 
     private ClawPowerState wheelRotationState;
 
+    private double armStartingPosition;
+
     public class ClawMovementAction implements Action {
-        private double leftClawServoDestination;// in degrees, where 0 is the starting degrees
-        private double rightClawServoDestination;
-
-        private final int directionleftClawServo;
-        private final int directionrightClawServo;
+//        private double leftClawServoDestination;// in degrees, where 0 is the starting degrees
+//        private double rightClawServoDestination;
+//
+//        private final int directionleftClawServo;
+//        private final int directionrightClawServo;
         private boolean isInitialized = false;
-
-        private final double power = 0.2;
-        private final double tolerance = 5; //in degrees, how much error can be accepted
+//        private final double tolerance = 2.5; //in degrees, how much error can be accepted
+        double destination;
+        long startTime;
         public ClawMovementAction(double destination) {
-            destination *= 0.714285;
-            double pos = lastPosRequest;
-            lastPosRequest = destination;
-            destination -= pos;
-
-            this.leftClawServoDestination = (getleftClawServoRotation() - destination);
-            this.rightClawServoDestination = (getrightClawServoRotation() - destination);
-
-            directionleftClawServo = getleftClawServoRotation() < leftClawServoDestination ? -1 : 1;
-            directionrightClawServo = getrightClawServoRotation() < rightClawServoDestination ? -1 : 1;
-
-            leftClawServoDestination = leftClawServoDestination % 360;
-            rightClawServoDestination = rightClawServoDestination % 360;
-
-        }
-
-//        public ClawMovementAction(double destination, Telemetry telemetry) {
+//            double pos = lastPosRequest;
+//            lastPosRequest = destination;
+//            destination -= pos;
+//
 //            this.leftClawServoDestination = (getleftClawServoRotation() - destination);
 //            this.rightClawServoDestination = (getrightClawServoRotation() - destination);
-//
-//            goUp =  destination > 0? 1 : -1;
 //
 //            directionleftClawServo = getleftClawServoRotation() < leftClawServoDestination ? -1 : 1;
 //            directionrightClawServo = getrightClawServoRotation() < rightClawServoDestination ? -1 : 1;
 //
 //            leftClawServoDestination = leftClawServoDestination % 360;
 //            rightClawServoDestination = rightClawServoDestination % 360;
-//        }
+            this.destination = destination;
+        }
+
 
         @Override
         public void preview(@NonNull Canvas fieldOverlay) {
@@ -89,18 +93,17 @@ public class DifferentialClaws {
 
         @Override
         public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            setArmTargetPosition(destination);
+            rotateArm(getPIDArmPower());
 
-            if (!isInitialized) {
-                leftClawServo.setPower(power * directionleftClawServo);
-                rightClawServo.setPower(power * directionrightClawServo);
+            if(!isInitialized){
                 isInitialized = true;
+                startTime = System.currentTimeMillis();
             }
-
-            if (abs(getleftClawServoRotation() - leftClawServoDestination) < tolerance || abs(getleftClawServoRotation() - leftClawServoDestination) < tolerance) {
-                rotateArm(holdingPower);
-                return false;
-            }
-            return true;
+            
+            updateLeftClawServoRotation();
+            updateRightClawServoRotation();
+            return System.currentTimeMillis() - startTime >= 1500;
         }
     }
 
@@ -189,7 +192,18 @@ public class DifferentialClaws {
         leftClawServo.setDirection(DcMotorSimple.Direction.FORWARD);
         rightClawServo.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        rotateWheels(0);
+        rotateArm(0.01);
+        rotateArm(0);
+
+        controller = new PIDController(p, i, d);
+        updateLeftClawServoRotation();
+        updateRightClawServoRotation();
+        rightClawStart = trueRightRotation;
+        leftClawStart = trueLeftRotation;
+        leftClawOldPos = leftClawStart;
+        rightClawOldPos = rightClawStart;
+        armStartingPosition = getArmPosition();
+
     }
 
     public enum ClawPowerState {
@@ -202,6 +216,60 @@ public class DifferentialClaws {
         ClawPowerState(double state) {this.state = state;}
     }
 
+    public double getArmPosition() {
+        double leftDiff = trueLeftRotation - leftClawStart;
+        double rightDiff = trueRightRotation - rightClawStart;
+
+        return Math.abs(((-rightDiff+leftDiff)/2)*1.4);
+    }
+
+    public static double getRotationOfInput(AnalogInput input) {
+        return (input.getVoltage() / input.getMaxVoltage()) * 360;
+    }
+
+    public void updateLeftClawServoRotation() {
+        double currentRotation = getRotationOfInput(clawInput1);
+        double diff = currentRotation - leftClawOldPos;
+
+        double newRotationEstimate = 180;
+        if(Math.abs(diff) > newRotationEstimate){
+            //new rotation occur
+            if(diff < 0)
+                diff += 360; //add rotation
+            else
+                diff -= 360; //minus rotation
+        }
+
+        leftClawOldPos = currentRotation;
+        trueLeftRotation += diff;
+    }
+
+    public void updateRightClawServoRotation() {
+        double currentRotation = getRotationOfInput(clawInput2);
+        double diff = currentRotation - rightClawOldPos;
+
+        double newRotationEstimate = 180;
+        if(Math.abs(diff) > newRotationEstimate){
+            //new rotation occur
+            if(diff < 0)
+                diff += 360; //add rotation
+            else
+                diff -= 360; //minus rotation
+        }
+
+        rightClawOldPos = currentRotation;
+        trueRightRotation += diff;
+    }
+    public double getActualArmRotation() {
+        return Math.max(getArmPosition() - armStartingPosition, armStartingPosition - getArmPosition());
+    }
+    public double getPIDArmPower(){
+        int armPos = (int)(getActualArmRotation());
+        double pid = controller.calculate(armPos, target);
+        double ff = Math.cos(Math.toRadians(target)) * f;
+
+        return pid + ff;
+    }
     public void rotateArm(double power){
         leftClawServo.setPower(power);
         rightClawServo.setPower(power);
@@ -218,6 +286,21 @@ public class DifferentialClaws {
         rightClawServo.setPower(-state.state);
     }
 
+    public void setArmTargetPosition(double pos){
+        if(pos >= target){
+            isGoingDown = false;
+        }else {
+            isGoingDown = true;
+        }
+        target = pos;
+    }
+
+    public double getArmTargetPosition(){
+        return target;
+    }
+    public double[] getServoVirtualPosition(){
+        return new double[] {trueLeftRotation, trueRightRotation};
+    }
     public void setPower(double p1, double p2){
         double sum = p1+p2;
         p1 /= sum;
@@ -273,5 +356,9 @@ public class DifferentialClaws {
 
     public HoldClawAndDropSampleAction test(double timeToHold, double timeToDrop) {
         return new HoldClawAndDropSampleAction(timeToHold, timeToDrop);
+    }
+
+    public ClawMovementAction clawMovementAction(double dest) {
+        return new ClawMovementAction(dest);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/ClawPIDFTuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/ClawPIDFTuning.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.tuning;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -10,153 +11,167 @@ import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+import org.firstinspires.ftc.teamcode.Systems.DifferentialClaws;
+
 @Config
 @TeleOp(name="ClawPIDFTuning")
 public class ClawPIDFTuning extends LinearOpMode {
-
-    private CRServo leftClawServo;
-    private CRServo rightClawServo;
-    private AnalogInput clawInput1;
-    private AnalogInput clawInput2;
-
-    private double leftClawStart;
-    private double rightClawStart;
-
-    private double leftClawOldPos;
-    private double rightClawOldPos;
-
-    private double trueLeftRotation = 0;
-    private double trueRightRotation = 0;
-
-
-    public double getArmPosition() {
-        double leftDiff = trueLeftRotation - leftClawStart;
-        double rightDiff = trueRightRotation - rightClawStart;
-
-        return Math.abs(((-rightDiff+leftDiff)/2)*1.4);
-    }
-
-    public static double getRotationOfInput(AnalogInput input) {
-        return (input.getVoltage() / input.getMaxVoltage()) * 360;
-    }
-
-    public double updateLeftClawServoRotation() {
-        double currentRotation = getRotationOfInput(clawInput1);
-        double diff = currentRotation - leftClawOldPos;
-
-        double newRotationEstimate = 180;
-        if(Math.abs(diff) > newRotationEstimate){
-            //new rotation occur
-            if(diff < 0)
-                diff += 360; //add rotation
-            else
-                diff -= 360; //minus rotation
-        }
-
-        leftClawOldPos = currentRotation;
-        trueLeftRotation += diff;
-        return trueLeftRotation;
-//        double curretposLeft = 360 - getRotationOfInput(clawInput1) +leftRotationNum*360;;
-//        if ((leftClawOldPos - curretposLeft) >= 300){
-//            leftRotationNum +=1;
-//        } else if ((leftClawOldPos - curretposLeft) <= -300) {
-//            leftRotationNum -=1;
+//
+//    private CRServo leftClawServo;
+//    private CRServo rightClawServo;
+//    private AnalogInput clawInput1;
+//    private AnalogInput clawInput2;
+//
+//    private double leftClawStart;
+//    private double rightClawStart;
+//
+//    private double leftClawOldPos;
+//    private double rightClawOldPos;
+//
+//    private double trueLeftRotation = 0;
+//    private double trueRightRotation = 0;
+//
+//
+//    public double getArmPosition() {
+//        double leftDiff = trueLeftRotation - leftClawStart;
+//        double rightDiff = trueRightRotation - rightClawStart;
+//
+//        return Math.abs(((-rightDiff+leftDiff)/2)*1.4);
+//    }
+//
+//    public static double getRotationOfInput(AnalogInput input) {
+//        return (input.getVoltage() / input.getMaxVoltage()) * 360;
+//    }
+//
+//    public double updateLeftClawServoRotation() {
+//        double currentRotation = getRotationOfInput(clawInput1);
+//        double diff = currentRotation - leftClawOldPos;
+//
+//        double newRotationEstimate = 180;
+//        if(Math.abs(diff) > newRotationEstimate){
+//            //new rotation occur
+//            if(diff < 0)
+//                diff += 360; //add rotation
+//            else
+//                diff -= 360; //minus rotation
 //        }
 //
-//        leftClawOldPos = 360 - curretposLeft +leftRotationNum*360;
-//        return leftClawOldPos;
-    }
-
-    public double updateRightClawServoRotation() {
-        double currentRotation = getRotationOfInput(clawInput2);
-        double diff = currentRotation - rightClawOldPos;
-
-        double newRotationEstimate = 180;
-        if(Math.abs(diff) > newRotationEstimate){
-            //new rotation occur
-            if(diff < 0)
-                diff += 360; //add rotation
-            else
-                diff -= 360; //minus rotation
-        }
-
-        rightClawOldPos = currentRotation;
-        trueRightRotation += diff;
-        return trueRightRotation;
-    }
-
-    public void rotateArm(double power){
-        leftClawServo.setPower(power);
-        rightClawServo.setPower(power);
-    }
-
-    public static double getActualArmRotation(double armStartingPosition, double currArmPosition) {
-        return Math.max(currArmPosition - armStartingPosition, armStartingPosition - currArmPosition);
-    }
-
-    private PIDController controller;
-
-    private double armStartingPosition;
-
-    public static double p = 0.02, i = 0.1, d = 0.0001;
-    public static double f = 0.01;
-
-    public static double target = 0;
+//        leftClawOldPos = currentRotation;
+//        trueLeftRotation += diff;
+//        return trueLeftRotation;
+////        double curretposLeft = 360 - getRotationOfInput(clawInput1) +leftRotationNum*360;;
+////        if ((leftClawOldPos - curretposLeft) >= 300){
+////            leftRotationNum +=1;
+////        } else if ((leftClawOldPos - curretposLeft) <= -300) {
+////            leftRotationNum -=1;
+////        }
+////
+////        leftClawOldPos = 360 - curretposLeft +leftRotationNum*360;
+////        return leftClawOldPos;
+//    }
+//
+//    public double updateRightClawServoRotation() {
+//        double currentRotation = getRotationOfInput(clawInput2);
+//        double diff = currentRotation - rightClawOldPos;
+//
+//        double newRotationEstimate = 180;
+//        if(Math.abs(diff) > newRotationEstimate){
+//            //new rotation occur
+//            if(diff < 0)
+//                diff += 360; //add rotation
+//            else
+//                diff -= 360; //minus rotation
+//        }
+//
+//        rightClawOldPos = currentRotation;
+//        trueRightRotation += diff;
+//        return trueRightRotation;
+//    }
+//
+//    public void rotateArm(double power){
+//        leftClawServo.setPower(power);
+//        rightClawServo.setPower(power);
+//    }
+//
+//    public static double getActualArmRotation(double armStartingPosition, double currArmPosition) {
+//        return Math.max(currArmPosition - armStartingPosition, armStartingPosition - currArmPosition);
+//    }
+//
+//    private PIDController controller;
+//
+//    private double armStartingPosition;
+//
+//    public static double p = 0.02, i = 0.01, d = 0.0001;
+//    public static double f = 0.1;
+//
+//    public static double target = 0;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        controller = new PIDController(p, i, d);
-        leftClawServo = this.hardwareMap.get(CRServo.class, "leftClawServo");
-        rightClawServo = this.hardwareMap.get(CRServo.class, "rightClawServo");
-        clawInput1 = this.hardwareMap.get(AnalogInput.class, "clawInput1");
-        clawInput2 = this.hardwareMap.get(AnalogInput.class, "clawInput2");
+        DifferentialClaws claws = new DifferentialClaws(this);
 
-        leftClawServo.setDirection(DcMotorSimple.Direction.FORWARD);
-        rightClawServo.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-
+//        controller = new PIDController(p, i, d);
+//        leftClawServo = this.hardwareMap.get(CRServo.class, "leftClawServo");
+//        rightClawServo = this.hardwareMap.get(CRServo.class, "rightClawServo");
+//        clawInput1 = this.hardwareMap.get(AnalogInput.class, "clawInput1");
+//        clawInput2 = this.hardwareMap.get(AnalogInput.class, "clawInput2");
+//
+//        leftClawServo.setDirection(DcMotorSimple.Direction.FORWARD);
+//        rightClawServo.setDirection(DcMotorSimple.Direction.REVERSE);
+//
+//        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+//
         waitForStart();
-
-        rotateArm(0.01);
-        rotateArm(0);
-
-        updateLeftClawServoRotation();
-        updateRightClawServoRotation();
-        rightClawStart = trueRightRotation;
-        leftClawStart = trueLeftRotation;
-        leftClawOldPos = leftClawStart;
-        rightClawOldPos = rightClawStart;
-        armStartingPosition = getArmPosition();
+//
+//        rotateArm(0.01);
+//        rotateArm(0);
+//
+//        updateLeftClawServoRotation();
+//        updateRightClawServoRotation();
+//        rightClawStart = trueRightRotation;
+//        leftClawStart = trueLeftRotation;
+//        leftClawOldPos = leftClawStart;
+//        rightClawOldPos = rightClawStart;
+//        armStartingPosition = getArmPosition();
 
         while (opModeIsActive()) {
-            controller.setPID(p, i, d);
-            int armPos = (int)(getActualArmRotation(armStartingPosition, getArmPosition()));
-            double pid = controller.calculate(armPos, target);
-            double ff = Math.cos(Math.toRadians(target)) * f;
+//            claws.controller.setPID(p, i, d);
+//            int armPos = (int)(getActualArmRotation(armStartingPosition, getArmPosition()));
+//            double pid = controller.calculate(armPos, target);
+//            double ff = Math.cos(Math.toRadians(target)) * f;
 
-            updateRightClawServoRotation();
-            updateLeftClawServoRotation();
-            double power = pid + ff;
+            claws.updateRightClawServoRotation();
+            claws.updateLeftClawServoRotation();
+            //double power = pid + ff;
 
-            rotateArm(power);
-            telemetry.addData("pos: ", armPos);
-            telemetry.addData("pid", pid);
-            telemetry.addData("ff", ff);
-            telemetry.addData("target: ", target);
-            telemetry.addData("starting rotation", armStartingPosition);
-            telemetry.addData("curr rotation", getArmPosition());
+            claws.rotateArm(claws.getPIDArmPower());
+            telemetry.addData("pos: ", claws.getActualArmRotation());
+            //telemetry.addData("pid", pid);
+            //telemetry.addData("ff", ff);
+            telemetry.addData("target: ", claws.getArmTargetPosition());
+            //telemetry.addData("starting rotation", armStartingPosition);
+            telemetry.addData("curr rotation", claws.getArmPosition());
             //telemetry.addData("left claw start", leftClawStart);
             //telemetry.addData("right claw start", rightClawStart);
             //telemetry.addData("left claw rotations", leftRotationNum);
             //telemetry.addData("right claw rotations", leftRotationNum);
-            telemetry.addData("right claw", trueRightRotation);
-            telemetry.addData("left claw", trueLeftRotation);
+            telemetry.addData("right claw", claws.getServoVirtualPosition()[1]);
+            telemetry.addData("left claw", claws.getServoVirtualPosition()[0]);
             //telemetry.addData("right claw old", rightClawOldPos);
             //telemetry.addData("left claw old", leftClawOldPos);
            // telemetry.addData("real left pos", getRotationOfInput(clawInput1));
 
             telemetry.update();
+
+            if (gamepad1.cross) {
+                claws.setArmTargetPosition(90);
+            }
+            else if (gamepad1.circle) {
+                claws.setArmTargetPosition(0);
+            }
+            else if (gamepad1.triangle) {
+                Actions.runBlocking(claws.clawMovementAction(40));
+            }
         }
     }
 }
