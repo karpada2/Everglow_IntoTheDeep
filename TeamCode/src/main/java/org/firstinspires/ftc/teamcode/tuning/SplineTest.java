d212bce535a3867521ee3a8a104d57bd0248903d
Omri7r <omri.r2007@gmail.com>
2025-03-17 10:42:29 +0200

    change group (not important)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
@@ -10,7 +10,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.TankDrive;
 @Disabled
-@TeleOp(name = "SplineTest")
+@TeleOp(name = "SplineTest", group = "Tests")
 public final class SplineTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
